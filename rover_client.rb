1c02ffcae79930b11be1c5c7c59aef27ff0f7c43
ShivonQ <realitykeeper@gmail.com>
2017-03-09 14:19:06 -0600

    Both Manifest and Images Calls work and produce good Hashes. Moving to Rails Server.

--- rover_client.rb
+++ rover_client.rb
@@ -1,19 +1,21 @@
 require 'http'
 require './key_file'
 class Rover_Client
-  attr_accessor :key
+  attr_accessor :key, :common_cams
+
   def initialize
     # it is ridiculous that I had to make this key a class.
     k=Api_Key.new
     self.key = k.key
-    puts self.key
-
+    self.common_cams=['FHAZ', 'RHAZ', 'NAVCAM']
   end
 
 
-  def get_images(rover_name,earth_date)
-    base_url = construct_uri(true,{rover_name:rover_name,earth_date:earth_date})
-    res=HTTParty.get(base_url, :verify=>false).parsed_response
+  def get_images(rover_name, earth_date)
+    base_url = construct_uri(true, {rover_name: rover_name, earth_date: earth_date})
+    res=HTTParty.get(base_url, :verify => false).parsed_response
+    strip_img_res(res,earth_date)
+
 
   end
 
@@ -34,17 +36,49 @@ class Rover_Client
   # end
 
   def get_manifest(rover_name)
-    base_url = construct_uri(false,{rover_name:rover_name})
-    res=HTTParty.get(base_url, :verify=>false).parsed_response
+    base_url = construct_uri(false, {rover_name: rover_name})
+    res=HTTParty.get(base_url, :verify => false).parsed_response
+    puts strip_manifest_res(res)
 
   end
 
-  def strip_img_res(res)
-
+  def strip_img_res(res,earth_date)
+  all_data = {
+      earth_date:earth_date,
+    cam_images:{
+      'Front Hazard Avoidance Camera'=> [],
+      'Rear Hazard Avoidance Camera'=> [],
+      'Mast Camera'=> [],
+      'Chemistry and Camera Complex'=>[],
+      'Mars Hand Lens Imager'=> [],
+      'Mars Descent Imager'=> [],
+      'Navigation Camera'=> [],
+      'Panoramic Camera'=> [],
+      'Miniature Thermal Emission Spectrometer (Mini-TES)'=> []
+    }
+  }
+  res['photos'].to_a.each do |item|
+    puts cam_name = item['camera']['full_name']
+    puts url = item['img_src']
+    all_data[:cam_images][cam_name]<<(url)
   end
+  all_data
+end
+
 
   def strip_manifest_res(res)
-
+    # TODO: Use this later for doing something with 'every day of its like an image taken'
+    # res['photo_manifest'].to_a.each do |piece|
+    #   puts piece
+    # end
+    manifest_data = {
+      'name'=>res['photo_manifest']['name'],
+      'landing_date'=>res['photo_manifest']['landing_date'],
+      'launch_date'=>res['photo_manifest']['launch_date'],
+      'status'=>res['photo_manifest']['status'],
+      'max_date'=>res['photo_manifest']['max_date'],
+      'total_photos'=>res['photo_manifest']['total_photos']
+    }
   end
 
   def construct_uri(is_image_call, options={})
