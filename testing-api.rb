eac102d1168f95b17e6cc5e0bd802f69e36e3339
ShivonQ <realitykeeper@gmail.com>
2017-03-14 15:37:48 -0500

    Added Max_Sol to manifest call, so people can see how many martian days theyve been on the surface functioning

--- testing-api.rb
+++ testing-api.rb
@@ -8,9 +8,11 @@ rc=Rover_Client.new
 # uri = rc.construct_uri(true,)
 # puts uri
 # str = {"id"=>617854, "sol"=>1625, "camera"=>{"id"=>26, "name"=>"NAVCAM", "rover_id"=>5, "full_name"=>"Navigation Camera"}, "img_src"=>"http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01625/opgs/edr/ncam/NLB_541745496EDR_F0611140NCAM00234M_.JPG", "earth_date"=>"2017-03-02", "rover"=>{"id"=>5, "name"=>"Curiosity", "landing_date"=>"2012-08-06", "launch_date"=>"2011-11-26", "status"=>"active", "max_sol"=>1629, "max_date"=>"2017-03-06", "total_photos"=>307282, "cameras"=>[{"name"=>"FHAZ", "full_name"=>"Front Hazard Avoidance Camera"}, {"name"=>"NAVCAM", "full_name"=>"Navigation Camera"}, {"name"=>"MAST", "full_name"=>"Mast Camera"}, {"name"=>"CHEMCAM", "full_name"=>"Chemistry and Camera Complex"}, {"name"=>"MAHLI", "full_name"=>"Mars Hand Lens Imager"}, {"name"=>"MARDI", "full_name"=>"Mars Descent Imager"}, {"name"=>"RHAZ", "full_name"=>"Rear Hazard Avoidance Camera"}]}}
-puts rc.get_images('opportunity','2012-03-03')
+# puts rc.get_images('opportunity','2012-03-03')
 # puts result
-# puts rc.get_manifest('curiosity')
+puts rc.get_manifest('curiosity')
+puts rc.get_manifest('opportunity')
+puts rc.get_manifest('spirit')
 # puts result
 #
 # str.each { |k, v|
